919932ea798dfcc9776820ea464fc7db73ddfe38
ramkumar12343 <bussinesswork2022@gmail.com>
2025-05-09 21:45:53 +0530

    Delete app/(auth) directory

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,7 +0,0 @@
-import AuthForm from "@/components/AuthForm";
-
-const SignIn = () => {
-  return <AuthForm type="sign-in" />
-}
-
-export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-import AuthForm from "@/components/AuthForm";
-
-const SignUp = () => {
-  return <AuthForm type="sign-up"/>
-}
-
-export default SignUp;
